41655eacb37df9e339a00812404d9e081e43540b
KingNNT <Dev.KingNNT@gmail.com>
2021-09-18 08:36:30 +0700

    UPDATE #

--- Makefiles/docker.mk
+++ Makefiles/docker.mk
@@ -1,8 +1,10 @@
-rm-image:
+docker-rm-image:
 	- docker rmi -f $(shell docker images -q)
-rm-container:
+docker-rm-container:
 	- docker rm $(shell docker ps -aq)
-rm-volume:
+docker-rm-volume:
 	- docker volume rm -f $(shell docker volume ls -q)
-rm-network:
+docker-rm-network:
 	- docker network rm $(shell docker network ls -q)
+docker-stop:
+	- docker stop $(docker ps -aq)
