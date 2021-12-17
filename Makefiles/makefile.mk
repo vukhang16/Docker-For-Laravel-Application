f2d50b1137cf42bed818a53d43d33729e02b7cf6
KingNNT <Dev.KingNNT@gmail.com>
2021-12-17 19:39:17 +0700

    UPDATE #

---
+++ Makefiles/makefile.mk
@@ -0,0 +1,2 @@
+list:
+	sh -c "$(MAKE) -p no_targets__ | awk -F':' '/^[a-zA-Z0-9][^\$$#\/\\t=]*:([^=]|$$)/ {split(\$$1,A,/ /);for(i in A)print A[i]}' | grep -v '__\$$' | sort"
--- shell/install/docker/ubuntu.sh
+++ shell/install/docker/ubuntu.sh
@@ -10,6 +10,8 @@ sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 sudo groupadd docker
 sudo usermod -aG docker $USER
 newgrp docker
+sudo systemctl enable docker.service
+sudo systemctl enable containerd.service
 if ! [ -x "$(command -v docker-compose)" ]; then
 	sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 	sudo chmod +x /usr/local/bin/docker-compose
